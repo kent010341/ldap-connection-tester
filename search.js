c5876ff33aa58b5a284b80881481104fb37f1ee7
kent.chen <kent.chen@hwacom.com>
2022-10-20 17:49:07 +0800

    add default port

--- search.js
+++ search.js
@@ -5,7 +5,20 @@ const config = require('config');
 // read config
 const scheme = config.get('connection.scheme');
 const host = config.get('connection.host');
-const port = config.get('connection.port');
+var port = config.get('connection.port');
+
+if (scheme != "ldap" && scheme != "ldaps") {
+  throw new SyntaxError("Scheme can only be either ldap or ldaps.");
+}
+
+// using default port
+if (port == "default") {
+  if (scheme == "ldap") {
+    port = 389;
+  } else if (scheme == "ldaps") {
+    port = 636;
+  }
+}
 
 const username = config.get('user.name');
 const userpassword = config.get('user.password');
